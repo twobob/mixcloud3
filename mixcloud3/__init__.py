0e0132c1a5853202f19562a8f9a8b009bb7ccf00
_ <simon.roberts@kaycare.co.uk>
2022-10-13 15:34:30 +0100

    Update __init__.py

--- mixcloud3/__init__.py
+++ mixcloud3/__init__.py
@@ -10,7 +10,29 @@ import requests
 import yaml
 from slugify import slugify
 
-from utils import logger
+
+import logging
+
+
+def logger(name=None, level=None):
+    """
+    A logger.
+    :param name: module name
+    :param level: debugging level
+    :return: a logger instance.
+    """
+
+    name = name if name else __name__
+    level = level if level else logging.INFO
+
+    _logger = logging.getLogger(name)
+    _logger.setLevel(level)
+    _hdlr = logging.StreamHandler()
+    _fmt = logging.Formatter("[%(asctime)s][%(levelname)s][%(name)s] %(message)s")
+    _hdlr.setFormatter(_fmt)
+    _logger.addHandler(_hdlr)
+
+    return _logger
 
 NETRC_MACHINE = "mixcloud-api"
 API_ROOT = "https://api.mixcloud.com"
